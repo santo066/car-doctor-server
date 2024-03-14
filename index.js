533314a6349b52be9c6e86b3350d70fce0e5ad78
santo066 <santomitro.kanaipur42@gmail.com>
2024-03-14 19:29:30 +0600

    env modfied

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(cookiePersser())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oyebmuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = process.env.MONGO_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
